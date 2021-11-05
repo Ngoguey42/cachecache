c4a754f53680aa8a1fb337057b7ec46f733abd61
Ngoguey42 <ngoguey@student.42.fr>
2021-11-05 21:04:34 +0100

    test

--- src/lru.ml
+++ src/lru.ml
@@ -8,8 +8,11 @@ module Make (K : Hashtbl.HashedType) = struct
   let unsafe_v c = { tbl = H.create c; lst = Dllist.create c dummy; cap = c }
 
   let v c =
+    Fmt.epr "> v %d\n%!" c;
     if c <= 0 then invalid_arg "capacity must be strictly positive";
-    unsafe_v c
+    let r = unsafe_v c in
+    Fmt.epr "  v out\n%!";
+    r
 
   let clear t =
     H.clear t.tbl;
@@ -22,7 +25,9 @@ module Make (K : Hashtbl.HashedType) = struct
   let mem t k = H.mem t.tbl k
 
   let find t k =
+    Fmt.epr "> find\n%!";
     let _index, value = H.find t.tbl k in
+    Fmt.epr "  find out\n%!";
     value
 
   let find_opt t k =
@@ -32,14 +37,29 @@ module Make (K : Hashtbl.HashedType) = struct
     with Not_found -> None
 
   let replace t k v =
+    Fmt.epr "> replace\n%!";
+    let res =
     try
+      Fmt.epr "  replace a\n%!";
       let index, _value = H.find t.tbl k in
+      Fmt.epr "  replace b\n%!";
       let new_index = Dllist.promote t.lst index in
-      H.replace t.tbl k (new_index, v)
+      Fmt.epr "  replace c\n%!";
+      let res = H.replace t.tbl k (new_index, v) in
+      Fmt.epr "  replace d\n%!";
+      res
     with Not_found ->
+      Fmt.epr "  replace 0\n%!";
       let index1, removed = Dllist.append t.lst k in
+      Fmt.epr "  replace 1\n%!";
       (match removed with None -> () | Some key -> H.remove t.tbl key);
-      H.replace t.tbl k (index1, v)
+      Fmt.epr "  replace 2\n%!";
+      let res = H.replace t.tbl k (index1, v) in
+      Fmt.epr "  replace 3\n%!";
+      res
+    in
+    Fmt.epr "  replace out\n%!";
+    res
 
   let remove t k =
     try
